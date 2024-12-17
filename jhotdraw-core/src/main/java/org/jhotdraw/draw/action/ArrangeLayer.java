c6b9c75008ecbc2be7af22a36a309bff5ffa5001
AndersBacon <Andth22@student.sdu.dk>
2024-12-17 15:33:41 +0100

    Arrange Layer interface
    Couldnt find one so i made one

---
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/ArrangeLayer.java
@@ -0,0 +1,8 @@
+package org.jhotdraw.draw.action;
+
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.draw.figure.Figure;
+
+public interface ArrangeLayer {
+    void execute(DrawingView view, Figure figure);
+}
