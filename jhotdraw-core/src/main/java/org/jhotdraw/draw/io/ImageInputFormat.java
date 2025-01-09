a7d5aac7f41a36e37357b26a0aed7c1b13a269fe
chjoe22 <chjoe22@student.sdu.dk>
2025-01-09 16:28:18 +0100

    Refactoring ImageInputFormat.java

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/io/ImageInputFormat.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/io/ImageInputFormat.java
@@ -1,20 +1,12 @@
-/*
- * @(#)ImageInputFormat.java
- *
- * Copyright (c) 1996-2010 The authors and contributors of JHotDraw.
- * You may not use, copy or modify this file, except in compliance with the
- * accompanying license terms.
- */
+// ImageInputFormat.java
 package org.jhotdraw.draw.io;
 
-import org.jhotdraw.draw.figure.Figure;
 import org.jhotdraw.draw.figure.ImageHolderFigure;
 import java.awt.*;
 import java.awt.datatransfer.*;
 import java.awt.geom.*;
 import java.io.*;
 import java.net.URI;
-import java.util.*;
 import javax.imageio.*;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -22,32 +14,6 @@ import org.jhotdraw.draw.*;
 import static org.jhotdraw.draw.AttributeKeys.*;
 import org.jhotdraw.util.Images;
 
-/**
- * An input format for importing drawings using one of the image formats
- * supported by javax.imageio.
- * <p>
- * This class uses the prototype design pattern. A ImageHolderFigure figure is
- * used as a prototype for creating a figure that holds the imported image.
- * <p>
- * If the drawing is replaced using the loaded image, the size of the
- * drawing is set to match the size of the image using the attributes
- * {@code AttributeKeys.CANVAS_WIDTH} and {@code AttributeKeys.CANVAS_HEIGHT}.
- *
- * <hr>
- * <b>Design Patterns</b>
- *
- * <p>
- * <em>Prototype</em><br>
- * The image input format creates new image holder figures by cloning a prototype figure
- * object and assigning an image to it, which was read from data input.
- * That's the reason why {@code Figure} extends the {@code Cloneable} interface.
- * <br>
- * Prototype: {@link org.jhotdraw.draw.ImageHolderFigure}; Client: {@link ImageInputFormat}.
- * <hr>
- *
- * @author Werner Randelshor
- * @version $Id$
- */
 public class ImageInputFormat implements InputFormat {
 
     /**
@@ -82,27 +48,26 @@ public class ImageInputFormat implements InputFormat {
     /**
      * Creates a new image input format for the specified image format.
      *
-     * @param formatName The format name for the javax.imageio.ImageIO object.
-     * @param description The format description to be used for the file filter.
+     * @param formatName    The format name for the javax.imageio.ImageIO object.
+     * @param description   The format description to be used for the file filter.
      * @param fileExtension The file extension to be used for the file filter.
-     * @param mimeType The mime type used for filtering data flavors from
-     * Transferable objects.
+     * @param mimeType      The mime type used for filtering data flavors from
+     *                      Transferable objects.
      */
-    public ImageInputFormat(ImageHolderFigure prototype, String formatName, String description, String fileExtension,
-            String mimeType) {
+    public ImageInputFormat(ImageHolderFigure prototype, String formatName, String description, String fileExtension, String mimeType) {
         this(prototype, formatName, description, new String[]{fileExtension}, new String[]{mimeType});
     }
 
     /**
      * Creates a new image input format for the specified image format.
      *
-     * @param formatName The format name for the javax.imageio.ImageIO object.
-     * @param description The format description to be used for the file filter.
+     * @param formatName     The format name for the javax.imageio.ImageIO object.
+     * @param description    The format description to be used for the file filter.
      * @param fileExtensions The file extensions to be used for the file filter.
-     * @param mimeTypes The mime typse used for filtering data flavors from
-     * Transferable objects.
+     * @param mimeTypes      The mime typse used for filtering data flavors from
+     *                       Transferable objects.
      */
-    public ImageInputFormat(ImageHolderFigure prototype, String formatName, String description, String fileExtensions[], String[] mimeTypes) {
+    public ImageInputFormat(ImageHolderFigure prototype, String formatName, String description, String[] fileExtensions, String[] mimeTypes) {
         this.prototype = prototype;
         this.formatName = formatName;
         this.description = description;
@@ -135,19 +100,8 @@ public class ImageInputFormat implements InputFormat {
     }
 
     public void read(File file, Drawing drawing, boolean replace) throws IOException {
-        ImageHolderFigure figure = (ImageHolderFigure) prototype.clone();
-        figure.loadImage(file);
-        figure.setBounds(
-                new Point2D.Double(0, 0),
-                new Point2D.Double(
-                        figure.getBufferedImage().getWidth(),
-                        figure.getBufferedImage().getHeight()));
-        if (replace) {
-            drawing.removeAllChildren();
-            drawing.set(CANVAS_WIDTH, figure.getBounds().width);
-            drawing.set(CANVAS_HEIGHT, figure.getBounds().height);
-        }
-        drawing.basicAdd(figure);
+        ImageHolderFigure figure = createImageHolder(file);
+        updateDrawing(drawing, figure, replace);
     }
 
     public void read(File file, Drawing drawing) throws IOException {
@@ -157,23 +111,38 @@ public class ImageInputFormat implements InputFormat {
     @Override
     public void read(InputStream in, Drawing drawing, boolean replace) throws IOException {
         ImageHolderFigure figure = createImageHolder(in);
+        updateDrawing(drawing, figure, replace);
+    }
+
+    private ImageHolderFigure createImageHolder(File file) throws IOException {
+        ImageHolderFigure figure = (ImageHolderFigure) prototype.clone();
+        figure.loadImage(file);
+        setFigureBounds(figure);
+        return figure;
+    }
+
+    private ImageHolderFigure createImageHolder(InputStream in) throws IOException {
+        ImageHolderFigure figure = (ImageHolderFigure) prototype.clone();
+        figure.loadImage(in);
+        setFigureBounds(figure);
+        return figure;
+    }
+
+    private void setFigureBounds(ImageHolderFigure figure) {
+        figure.setBounds(
+                new Point2D.Double(0, 0),
+                new Point2D.Double(
+                        figure.getBufferedImage().getWidth(),
+                        figure.getBufferedImage().getHeight()));
+    }
+
+    private void updateDrawing(Drawing drawing, ImageHolderFigure figure, boolean replace) {
         if (replace) {
             drawing.removeAllChildren();
             drawing.set(CANVAS_WIDTH, figure.getBounds().width);
             drawing.set(CANVAS_HEIGHT, figure.getBounds().height);
         }
         drawing.basicAdd(figure);
-    }
-
-    public ImageHolderFigure createImageHolder(InputStream in) throws IOException {
-        ImageHolderFigure figure = (ImageHolderFigure) prototype.clone();
-        figure.loadImage(in);
-        figure.setBounds(
-                new Point2D.Double(0, 0),
-                new Point2D.Double(
-                        figure.getBufferedImage().getWidth(),
-                        figure.getBufferedImage().getHeight()));
-        return figure;
     }
 
     @Override
@@ -191,44 +160,40 @@ public class ImageInputFormat implements InputFormat {
 
     @Override
     public void read(Transferable t, Drawing drawing, boolean replace) throws UnsupportedFlavorException, IOException {
-        DataFlavor importFlavor = null;
-        SearchLoop:
+        DataFlavor importFlavor = findImportFlavor(t);
+        Object data = t.getTransferData(importFlavor);
+        Image img = extractImage(data);
+        ImageHolderFigure figure = createImageHolder(img);
+        updateDrawing(drawing, figure, replace);
+    }
+
+    private DataFlavor findImportFlavor(Transferable t) throws UnsupportedFlavorException {
         for (DataFlavor flavor : t.getTransferDataFlavors()) {
             if (DataFlavor.imageFlavor.match(flavor)) {
-                importFlavor = flavor;
-                break SearchLoop;
+                return flavor;
             }
             for (String mimeType : mimeTypes) {
                 if (flavor.isMimeTypeEqual(mimeType)) {
-                    importFlavor = flavor;
-                    break SearchLoop;
+                    return flavor;
                 }
             }
         }
-        Object data = t.getTransferData(importFlavor);
-        Image img = null;
+        throw new UnsupportedFlavorException(null);
+    }
+
+    private Image extractImage(Object data) throws IOException {
         if (data instanceof Image) {
-            img = (Image) data;
+            return (Image) data;
         } else if (data instanceof InputStream) {
-            img = ImageIO.read((InputStream) data);
+            return ImageIO.read((InputStream) data);
         }
-        if (img == null) {
-            throw new IOException("Unsupported data format " + importFlavor);
-        }
+        throw new IOException("Unsupported data format");
+    }
+
+    private ImageHolderFigure createImageHolder(Image img) {
         ImageHolderFigure figure = (ImageHolderFigure) prototype.clone();
         figure.setBufferedImage(Images.toBufferedImage(img));
-        figure.setBounds(
-                new Point2D.Double(0, 0),
-                new Point2D.Double(
-                        figure.getBufferedImage().getWidth(),
-                        figure.getBufferedImage().getHeight()));
-        LinkedList<Figure> list = new LinkedList<>();
-        list.add(figure);
-        if (replace) {
-            drawing.removeAllChildren();
-            drawing.set(CANVAS_WIDTH, figure.getBounds().width);
-            drawing.set(CANVAS_HEIGHT, figure.getBounds().height);
-        }
-        drawing.addAll(list);
+        setFigureBounds(figure);
+        return figure;
     }
 }
